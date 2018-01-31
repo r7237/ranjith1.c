c75d5cfb4de225960f58cb45aa5686ab843564c9
r7237 <35914754+r7237@users.noreply.github.com>
2018-01-31 14:46:30 +0530

    Create READM.c

---
+++ READM.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the values of a,b");
+scanf("%d%d,&a&b);
+printf("enters %d,a<=b");
+printf("values %d,a>=b");
+printf("%d",a);
+return 0;
+}
